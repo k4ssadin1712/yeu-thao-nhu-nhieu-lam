9527b84942bf201bf870e094b24b99ac895610e5
k4ssadin1712 <121285768+k4ssadin1712@users.noreply.github.com>
2022-12-23 16:18:29 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Nguyễn Ngọc Thảo Như';
+const giftUrl = 'https://www.facebook.com/ngnhu26';
+const eventName = 'My Love';
+const titleCard = 'Tặng Em';
+const contentCard = 'Cảm ơn em đã đến bên anh, từ khi có em anh từ một thằng nhóc tiêu cực không quan tâm đến mọi người xung quanh trở nên yêu đời và biết quan tâm đến người mình yêu. Em đã trải qua nhiều đau khổ do người cũ gây ra. Yên tâm đi anh sẽ yêu thương em và không khiến em phải buồn đâu nha <3. Nguyện cả đời này yêu Em <3';
+const giftImage = 'bae.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
